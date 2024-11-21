4c0073f19053ebd478fc5ec8100112102ed7fcb6
littlecodingdrago <allthestuffilove@protonmail.com>
2024-11-21 21:50:27 +0100

    Implement server-sent events for real-time player updates and add socket.io support

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+
 app.use(express.json());
 
 let players = [
@@ -14,9 +17,6 @@ let activePlayer = null;
 let isPaused = false;
 
 app.use(express.static(path.join(__dirname, '../public')));
-
-// Serve socket.io client script
-app.use('/socket.io', express.static(path.join(__dirname, '../node_modules/socket.io/client-dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/Session_Timer_Manager.html'));
@@ -32,7 +32,45 @@ app.post('/api/updateData', (req, res) => {
     players = req.body.players;
     activePlayer = req.body.activePlayer;
     isPaused = req.body.isPaused;
+    broadcastUpdate();
     res.sendStatus(200);
 });
 
-module.exports = app;
+app.get('/api/updates', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+
+    const sendUpdate = () => {
+        res.write(`data: ${JSON.stringify({ players, activePlayer, isPaused })}\n\n`);
+    };
+
+    sendUpdate();
+    const intervalId = setInterval(sendUpdate, 1000);
+
+    req.on('close', () => {
+        clearInterval(intervalId);
+    });
+});
+
+function updateTimers() {
+    if (isPaused) return; // Do not update timers if paused
+
+    const now = Date.now();
+    players.forEach(player => {
+        if (player.startTime) {
+            player.currentActiveTime = now - player.startTime;
+        } else {
+            player.waitTime += 1000;
+        }
+    });
+    broadcastUpdate();
+}
+
+function broadcastUpdate() {
+    // This function will be used to broadcast updates to all connected clients
+}
+
+setInterval(updateTimers, 1000);
+
+module.exports = server;
